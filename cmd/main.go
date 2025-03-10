e46876dfa8a96b746f7b516d99b1d45573a2237b
solve <solve.sthor@gmail.com>
2025-03-10 16:09:19 +0100

    chore: updated with propper cron expressions.

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,22 +10,20 @@ import (
 )
 
 func main() {
-	// Example: Parse cron expression and create a ticker
-	// cronExpr := "5 * *" // Every 5 seconds
-	// ticker, err := ConvertCronExpressionToTicker(cronExpr)
-
-	// Capture OS signals
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	// Use the ticker in a loop with select
-	for {
-		select {
-		case sig := <-sigs:
-			fmt.Println("Received signal:", sig)
-			fmt.Println("Shutting down gracefully...")
-			return
-		}
-	}
-	cron.New()
+	cron := cron.New()
+	l := "TZ=UTC @every 5s"
+	cron.AddFunc(l, testFunc)
+	go func() {
+		<-sigs
+		cron.Stop()
+	}()
+	cron.Run()
+}
+
+func testFunc() {
+	log.Println("hey")
 }
